26d5b8f5a841030be098714e2980f5092be4a263
ryandezall <ryandezall@the-black-pearl.lan>
2026-01-25 19:30:22 -0700

    Created main menu and added displayMenu() to MovieManager.

--- src/sait/mms/application/AppDriver.java
+++ src/sait/mms/application/AppDriver.java
@@ -1,13 +1,52 @@
 package sait.mms.application;
-
+ 
+import java.util.Scanner;
 import sait.mms.manager.MovieManager;
 import sait.mms.problemdomain.Movie;
 
 public class AppDriver {
+	
+	static MovieManager m = new MovieManager();
+	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			// main menu options
+			System.out.println("Welcome to the Movie Management System!");
+			System.out.println("Please select from the following options:");
+			System.out.println("1: Add New Movie and Save.");
+			System.out.println("2: Generate List of Movies Made in a Year.");
+			System.out.println("3: Generate List of Random Movies.");
+			System.out.println("4: Exit Program.\n");
+			
+			// user input and choice
+			System.out.print("Please Enter Choice: ");
+			int choice = sc.nextInt();
+			
+			// input results
+			if (choice == 1) {
+				// these have not been made yet
+				m.addMovie();
+				m.saveMovieListToFiles();
+			}
+			else if (choice == 2) {
+				m.generateMovieListInYear(); // not made
+			}
+			else if (choice == 3) {
+				m.generateRandomMovieList(); // not made
+			}
+			else if (choice == 4) {
+				System.out.println("Exiting Program.");
+				break;
+			}
+			else {
+				System.out.println("Please Enter Valid Input.");
+			}
+		
+		}
 		// The System.out.println below is just to confirm that the app is running normally.
-		System.out.println("HELLO WORLD FROM AppDriver.Java! This output confirms it's working!");
-		MovieManager.manager_show();
-		Movie.movie_call_method();
 	}
 }
+	
--- src/sait/mms/manager/MovieManager.java
+++ src/sait/mms/manager/MovieManager.java
@@ -2,7 +2,19 @@ package sait.mms.manager;
 
 public class MovieManager {
 	public static void manager_show() {
+	
+	}
+		
+	// I already made this while making the AppDriver    so I just changed it's location to here to be able to call the function.
+	public static void displayMenu() {
+		System.out.println("Welcome to the Movie Management System!");
+		System.out.println("Please select from the following options:");
+		System.out.println("1: Add New Movie and Save.");
+		System.out.println("2: Generate List of Movies Made in a Year.");
+		System.out.println("3: Generate List of Random Movies.");
+		System.out.println("4: Exit Program.\n");	
+		}
 		// Initial test output to confirm the package and class is working
 		System.out.println("Hello World from MovieManager.java! This just confirms the function is successfully called!");
-	}
+
 }
